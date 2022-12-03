46c7bf3a6b4cf175c37561a74b140921c9b943fd
Santiago Avila <santiavilag2015@gmail.com>
2022-12-03 00:33:10 -0500

    Added gif of enchantment table

--- commands/encrypt.js
+++ commands/encrypt.js
@@ -1,8 +1,9 @@
 import boxen from 'boxen';
 import chalk from 'chalk'
 import {abc,sga} from './utils.js'
+import terminalImage from 'terminal-image';
 
-const encrypt = sentence => {
+const encrypt = async sentence => {
     const words = sentence.split(" ");
     let encodedSentence = "";
     words.forEach(word => {
@@ -12,6 +13,7 @@ const encrypt = sentence => {
         }
         encodedSentence += " ";
     })
+    console.log(await terminalImage.gifFile('enchantmentTable.gif', {width: '50%', height: '50%'}));
     console.log("\n" + boxen(chalk.green("\n" + encodedSentence + "\n"), {padding: 1, borderColor: 'green', dimBorder: true}) + "\n");
 }
 
